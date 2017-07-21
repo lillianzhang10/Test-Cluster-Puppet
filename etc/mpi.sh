52876130ac2ded220a7220b1ed7764a9f2f05dd7
root <maivang.tran@gmail.com>
2017-07-21 13:48:02 -0700

    first commit

---
+++ etc/mpi.sh
@@ -0,0 +1,2 @@
+export PATH=/usr/lib64/openmpi/bin:/usr/lib64/openmpi/bin:/usr/lib64/openmpi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib:/usr/lib64/openmpi/lib:/usr/lib64/openmpi/lib:
